fd2443493688bdef20a938bba7cbf1ef13885945
Enes Kocakaya <92929776+justenes@users.noreply.github.com>
2024-04-08 13:54:46 +0200

    Delete Zadanie 3 directory

--- Zadanie 3/app.js
+++
@@ -1,6 +0,0 @@
-const student = require("./student");
-
-const FULLNAME = student.getStudentFullName();
-const studentID = student.getStudentId();
-
-console.log(`My name is ${FULLNAME}. My student ID is ${STUDENT_ID}`);
